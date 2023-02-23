ba46d732086ff7bf7ef70644d959562accd951a8
sylvie <1553902582@qq.com>
2023-02-23 10:08:18 +0800

    第四次

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  publicPath: "./",
+  productionSourceMap: false,
   css: { extract: false },
 });
 // module.exports = {
