9a07fd15e11210f264bbe6cc29cd8af55e4caba6
LGD-PEWS <70420674+LGD-PEWS@users.noreply.github.com>
2023-02-23 09:13:19 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    在这里输入
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+
+  }
+}
+</script>
+
+
+<style scoped></style>
